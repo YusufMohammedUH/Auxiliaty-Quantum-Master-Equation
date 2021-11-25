399bd46605c4c36f00581b0ba1f899b34da075b4
YusufM <yusuf.mohammed@physik.uni-hamburg.de>
2021-11-25 14:23:53 +0100

    Add Tests

--- tests/test_frequency_greens_function.py
+++ tests/test_frequency_greens_function.py
@@ -3,5 +3,28 @@ import numpy as np
 from src import frequency_greens_function as fg
 
 
+def test_FrequencySystem_freq_raise_TypeError():
+    ws = [i for i in range(10)]
+    with pytest.raises(TypeError):
+        fg.FrequencyGreen(ws)
+
+
+def test_FrequencySystem_retarded_raise_ValueError():
+    ws = [i for i in range(10)]
+    with pytest.raises(ValueError):
+        fg.FrequencyGreen(np.array(ws), np.array(ws[2:]))
+
+
+def test_FrequencySystem_retarded_raise_ValueError():
+    ws = [i for i in range(10)]
+    with pytest.raises(ValueError):
+        fg.FrequencyGreen(np.array(ws), np.array(ws[2:]))
+
+# TODO: 1. write test for dyson function
+#       2.       -"-      set_green_from_auxiliary
+#       3.       -"-      get_self_enerqy
+
+
 if __name__ == "__main__":
-    pytest.main("-v test_auxiliary_hybridization_function.py")
+
+    pytest.main("-v test_frequency_greens_function.py")
