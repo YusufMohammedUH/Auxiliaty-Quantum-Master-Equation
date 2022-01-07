Bug fix sign of  hopping  in hopping matrix set to +1 instead of 1

examples/two_orbital_spinless_fermions.py
@@ -12,7 +12,7 @@ import matplotlib.pyplot as plt
 # ########################### Unitary propagation #############################
 # Local Hamiltonian parameters of two site Problem
 nsite = 2
-ts = -1.0 * np.ones(nsite - 1)
+ts = 1.0 * np.ones(nsite - 1)
 U = np.zeros(nsite)
 es = np.array([0., 1.])
 
@@ -97,7 +97,7 @@ plt.show()
 
 # ##################### Unitary And Incoherent Dynamics #######################
 
-ts = -1.0 * np.ones(nsite - 1)
+ts = 1.0 * np.ones(nsite - 1)
 
 # Setting hopping matrix
 Tmat = ham.get_1D_chain_nearest_neighbor_hopping_matrix(nsite, es, ts)

src/model_hamiltonian.py
@@ -69,7 +69,7 @@ def get_1D_chain_nearest_neighbor_hopping_matrix(nsite, es, ts, boundary=None):
 
     Tmat = np.zeros((nsite, nsite))
 
-    Tmat = np.diag(es) + np.diag(np.conj(-ts), k=-1) + np.diag(-ts, k=1)
+    Tmat = np.diag(es) + np.diag(np.conj(ts), k=-1) + np.diag(ts, k=1)
 
     if boundary is not None:
         Tmat[0, - 1] = - t * boundary