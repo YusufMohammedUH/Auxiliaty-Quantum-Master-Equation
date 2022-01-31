acf5c3a823f22dec9bbbc5d7d66e656174b01e65
YusufM <yusuf.mohammed@physik.uni-hamburg.de>
2022-01-31 18:36:32 +0100

    Refactor by using jit

--- src/dos_util.py
+++ src/dos_util.py
@@ -1,14 +1,16 @@
 import numpy as np
+from numba import njit
 import src.frequency_greens_function as fg
 
 
+@njit(cache=True)
 def flat_bath_retarded(w, e0, D, gamma):
     x = D + (w - e0)
     y = (w - e0 - D)
     if x == 0:
-        real_part = -float("inf")
+        real_part = -np.inf
     elif y == 0:
-        real_part = -float("-inf")
+        real_part = np.inf
     else:
         real_part = (-gamma / np.pi) * np.log(np.abs(y / x))
 
@@ -18,17 +20,19 @@ def flat_bath_retarded(w, e0, D, gamma):
     return complex(real_part, imag_part)
 
 
+@njit(cache=True)
 def lorenzian_bath_retarded(w, e0, gamma, v=1):
     x = w - e0 - 1j * gamma
     y = (w - e0)**2 + gamma**2
     result = 0
     if y == 0:
-        result = complex(0, float("-inf"))
+        result = complex(0, -np.inf)
     else:
         result = x / y
     return (v**2) * result
 
 
+@njit(cache=True)
 def heaviside(x, x0):
     if x < x0:
         return 0.
@@ -36,6 +40,7 @@ def heaviside(x, x0):
         return 1.
 
 
+@njit(cache=True)
 def fermi(e, e0, mu, beta):
     x = (e + e0 - mu) * beta
     if x < 0.0:
@@ -44,7 +49,8 @@ def fermi(e, e0, mu, beta):
         return 1.0 - 1.0 / (1.0 + np.exp(-x))
 
 
-def set_hybridization(freq, retarded_function, *args):
+@njit(cache=True)
+def _set_hybridization(freq, retarded_function, *args):
     e0 = args[0]
     mu = args[1]
     beta = args[2]
@@ -52,4 +58,9 @@ def set_hybridization(freq, retarded_function, *args):
     keldysh = np.array([1j * (1 - 2 * fermi(f, e0, mu, beta)) *
                         np.imag(retarded_function(f, e0, *args[3:]))
                         for f in freq])
+    return retarded, keldysh
+
+
+def set_hybridization(freq, retarded_function, *args):
+    retarded, keldysh = _set_hybridization(freq, retarded_function, *args)
     return fg.FrequencyGreen(freq=freq, retarded=retarded, keldysh=keldysh)
