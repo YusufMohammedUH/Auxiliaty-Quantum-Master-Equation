Bug fix calculation of two single greens function
sign in get_two_point_correlator_frequency was wrong +
get_single_particle_green_physical components

src/greens_function/correlation_functions.py
@@ -347,29 +347,19 @@ class Correlators:
             self.get_single_particle_green((1, 0), freq, sites, spin)
         green_lesser_plus, green_lesser_minus = \
             self.get_single_particle_green((0, 1), freq, sites, spin)
-        green_aux_R = green_greater_plus - green_lesser_plus
-        green_aux_K = green_greater_plus + green_greater_minus \
-            + green_lesser_plus + green_lesser_minus
-        if keldysh_comp == 'keldysh':
-            return fg.FrequencyGreen(
-                freq, retarded=green_aux_R, keldysh=green_aux_K,
-                keldysh_comp=keldysh_comp)
+
+        green_aux_R = 0.5 * (green_greater_plus + green_greater_minus - (
+            green_lesser_plus + green_lesser_minus))
+
+        if keldysh_comp == "keldysh":
+            green_aux_K = green_greater_plus + green_greater_minus \
+                + green_lesser_plus + green_lesser_minus
         elif keldysh_comp == 'lesser':
-            g_tmp = fg.FrequencyGreen(
-                freq, retarded=green_aux_R, keldysh=green_aux_K,
-                keldysh_comp='keldysh')
-            g_tmp.keldysh = g_tmp.get_lesser()
-            g_tmp.keldysh_comp = 'lesser'
-            return g_tmp
-        # green_aux_R = green_greater_plus + green_greater_minus - (
-        #     green_lesser_plus + green_lesser_minus)
-        # green_aux_R *= 0.5
-        # if keldysh_comp == "keldysh":
-        #     green_aux_K = green_greater_plus + green_greater_minus \
-        #         + green_lesser_plus + green_lesser_minus
-        # elif keldysh_comp == 'lesser':
-        #     green_aux_K = green_lesser_plus + green_lesser_minus
-        #     # green_aux_K *= (1. / (2))
+            green_aux_K = green_lesser_plus + green_lesser_minus
+            green_aux_K *= (1. / (2 * np.pi))
+        return fg.FrequencyGreen(
+            freq, retarded=green_aux_R, keldysh=green_aux_K,
+            keldysh_comp=keldysh_comp)
 
     def get_susceptibility(self, freq: np.ndarray, component: Tuple[int, int],
                            channels: Tuple[str, str],
@@ -455,14 +445,12 @@ class Correlators:
         out: fg.FrequencyGreen
             Susceptibility on the physical contour
         """
-        chi_greater_plus, chi_greater_minus = \
-            self.get_susceptibility(freq=freq, component=(1, 0),
-                                    channels=channels,
-                                    sites=sites, prefactor=prefactor)
-        chi_lesser_plus, chi_lesser_minus = \
-            self.get_susceptibility(freq=freq, component=(0, 1),
-                                    channels=channels,
-                                    sites=sites, prefactor=prefactor)
+        chi_greater_plus, chi_greater_minus = self.get_susceptibility(freq=freq, component=(1, 0),
+                                                                      channels=channels,
+                                                                      sites=sites, prefactor=prefactor)
+        chi_lesser_plus, chi_lesser_minus = self.get_susceptibility(freq=freq, component=(0, 1),
+                                                                    channels=channels,
+                                                                    sites=sites, prefactor=prefactor)
 
         chi_aux_R = chi_greater_plus - chi_lesser_plus
         chi_aux_K = chi_greater_plus + chi_greater_minus + chi_lesser_plus \
@@ -579,20 +567,18 @@ class Correlators:
 
         if not return_:
             for component in self.correlators[3][spin]:
-                self.correlators[3][spin][component] = \
-                    self.get_three_point_vertex_components(
-                        component=component, freq=freq, sites=sites, spin=spin,
-                        permutation_sign=permutation_sign,
-                        prefactor=prefactor)
+                self.correlators[3][spin][component] = self.get_three_point_vertex_components(
+                    component=component, freq=freq, sites=sites, spin=spin,
+                    permutation_sign=permutation_sign,
+                    prefactor=prefactor)
         else:
             three_point_vertex = np.zeros(
                 (freq.shape[0], freq.shape[0], 2, 2, 2),
                 dtype=np.complex128)
             for i, j, k in self.correlators[3][
                     ("up", "up", 'ch')]:
-                three_point_vertex[:, :, i, j, k] = \
-                    self.get_three_point_vertex_components(
-                        component=(i, j, k), freq=freq, sites=sites, spin=spin,
+                three_point_vertex[:, :, i, j, k] = self.get_three_point_vertex_components(
+                    component=(i, j, k), freq=freq, sites=sites, spin=spin,
                     permutation_sign=permutation_sign, prefactor=prefactor)
 
             return three_point_vertex
@@ -644,8 +630,7 @@ class Correlators:
                                             ((0, 0), (1, 1))],
                                            n_correlator_symmetrie)
             if n == 3 and trilex:
-                self.contour_symmetries[3] = \
-                    n_correlator_symmetrie.copy()
+                self.contour_symmetries[3] = n_correlator_symmetrie.copy()
             else:
                 self.contour_symmetries[n] = n_correlator_symmetrie.copy()
 

src/liouville_solvers/ed_solver_get_correlator_jit.py
@@ -64,17 +64,19 @@ def get_two_point_correlator_frequency(green_component_plus: np.ndarray,
             L_n = vals_sectors[0][n]
             if np.abs(L_n) > e_cut_off:
 
-                G_plus -= (precalc_correlators[0][n] /
+                G_plus += (precalc_correlators[0][n] /
                            ((1j * freq[i] + L_n)))
 
         for n in prange(tensor_shapes[1][0]):
             L_n = vals_sectors[1][n]
             if np.abs(L_n) > e_cut_off:
-                G_minus += (precalc_correlators[1][n] /
+                G_minus -= (precalc_correlators[1][n] /
                             ((1j * freq[i] - L_n)))
 
-        green_component_plus[i] = prefactor * permutation_sign[0] * G_plus
-        green_component_minus[i] = prefactor * permutation_sign[1] * G_minus
+        green_component_plus[i] = (-1.0 + 0.0j) * \
+            prefactor * permutation_sign[0] * G_plus
+        green_component_minus[i] = np.conj(
+            prefactor * permutation_sign[1] * G_minus)
 
 
 @njit(parallel=True, cache=True)